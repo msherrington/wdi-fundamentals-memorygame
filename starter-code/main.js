419dc9260d85fcae8f16efb076d2ff30433e78cf
Mark S <thewayforward3000@gmail.com>
2017-01-15 16:22:30 +0000

    created pop-up alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,25 @@
 console.log("JS file is connected to HTML! Woo!")
 
+// variables to name the playing cards
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+//compares if 2 cards are equal
+if  (cardTwo === cardFour){
+	var match = true;
+} else {
+	var match = false;
+}
+
+//alerts whether cards are equal or not
+switch (match) {
+ case true:
+    alert('Your cards match!');
+    break;
+ case false:
+    alert('Better luck next time.');
+    break;
+ }
+
