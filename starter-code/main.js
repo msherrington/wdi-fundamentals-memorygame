e43a4b00c6d4c2b759078037db6abaa9234b6236
Mark S <thewayforward3000@gmail.com>
2017-01-17 20:50:29 +0000

    removed playing cards from html, recreated in javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,6 +6,7 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*
 //compares if 2 cards are equal
 if  (cardTwo === cardFour){
 	var match = true;
@@ -22,4 +23,18 @@ switch (match) {
     alert('Better luck next time.');
     break;
  }
+ */
+
+//variable to find board div by id
+var gameBoard = document.getElementById('game-board');
+
+//create 4 x card divs with 'card' class name
+var createCards = function() {
+	for (var i = 0; i <= 3; i++) {
+		var cards = document.createElement('div');
+		cards.className = 'card';
+		document.querySelector('div').appendChild(cards);
+	}
+}
+createCards();
 
