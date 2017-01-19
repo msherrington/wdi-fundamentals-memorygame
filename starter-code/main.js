ee83caedfdc420247d23961f1a87cab95c7c3ac6
Mark S <thewayforward3000@gmail.com>
2017-01-19 22:16:11 +0000

    finished Unit 11 for submission

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,41 +1,59 @@
 console.log("JS file is connected to HTML! Woo!")
 
-// variables to name the playing cards
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//array containing card names
+var cards = ['queen', 'queen', 'king', 'king'];
+//empty array for cards in play
+var cardsInPlay = [];
 
-/*
-//compares if 2 cards are equal
-if  (cardTwo === cardFour){
-	var match = true;
-} else {
-	var match = false;
-}
+function createBoard(number) {
+    console.log(cards);
+    for (var i = 0; i < number; i++) {
+        //create a new div for each card
+        var newCard = document.createElement('div');
+        //assign each new div a class of 'card'
+        newCard.setAttribute('class', 'card');
+        //set each card to be an element of the cards array
+        newCard.setAttribute('data-card', cards[i]);
+        //add each new card div as a child of the game board div
+        document.getElementById('game-board').appendChild(newCard);
+        // when a card is clicked the function isTwoCards will be executed
+        newCard.addEventListener('click', isTwoCards);
+} 
+}   
+//run createboard function for no. of cards in array (4)
+createBoard(cards.length);
 
-//alerts whether cards are equal or not
-switch (match) {
- case true:
-    alert('Your cards match!');
-    break;
- case false:
-    alert('Better luck next time.');
-    break;
- }
- */
 
-//variable to find board div by id
-var gameBoard = document.getElementById('game-board');
-
-//create 4 x card divs with 'card' class name
-var createCards = function() {
-	for (var i = 0; i <= 3; i++) {
-		var cards = document.createElement('div');
-		cards.className = 'card';
-		document.querySelector('div').appendChild(cards);
+//alert function to see if cards match or not
+function isMatch(cards) {
+	if (cards[0] === cards[1]){
+		alert('You got a match!');
+	} else {
+		alert('Better luck next time.');
 	}
 }
-createCards();
 
 
+//check to see if there are 2 cards in play
+function isTwoCards() {
+  // add card to array of cards in play
+  cardsInPlay.push(this.getAttribute('data-card'));
+  //put image on cards
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='king.jpg'>";
+	} else {
+		this.innerHTML = "<img src='queen.jpg'>";
+	}
+   // if you have two cards in play, check for a match
+  if (cardsInPlay.length === 2) {
+    // pass the cardsInPlay as an argument to the isMatch function
+    isMatch(cardsInPlay);
+    // clear cards in play array for your next try
+    cardsInPlay = [];
+    
+    //was trying to clear card images here but couldn't get it working properly
+    //console.log(this.getAttribute('data-card'));
+    //this.innerHTML = "<img />";
+  }
+}
